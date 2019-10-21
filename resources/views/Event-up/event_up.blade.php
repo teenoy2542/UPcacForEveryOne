caf985edf1c3a03dc38ac6654ced5fd070505954
StrangerIX <gill.momoo@gmail.com>
2019-10-21 23:39:24 +0700

    [Insert] add event_up

---
+++ resources/views/Event-up/event_up.blade.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+        <h5 class="text-white h4">Collapsed content</h5>
+        <span class="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card-body">
+                <div class="col-md">
+                    <div class="card">
+                        <div class="card-header text-center">รถที่จะขึ้นมอ</div>
+                        <div class="card-body">
+                            <div class="form-group" >
+                                <label>สวัสดีครับท่านผู้ชม</label>
+                            </div>
+                        </div>
+                            
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/driver', 'DriverController');
+
+Route::get('/event-up', function () {
+    return view('Event-up.event_up');
+});
